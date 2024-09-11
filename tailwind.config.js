cbb85eb7af85774fdc0c5d72b3394e01936b0e7e
Rizky Khoiruddin <rizky.khoiruddin17@gmail.com>
2024-09-11 16:34:24 +0700

    Update v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
+        colors: {
+            "black-primary": "#000000",
+            "black-secondary": "#1E1F20",
+            "white-primary": "#FFFFFF",
+            "white-secondary": "#F7F7F7",
+            "gray-primary": "#707070",
+            "gray-secondary": "#CCCCCC",
+            "blue": "#1668E8",
+            "green": "#C7F2AB",
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
